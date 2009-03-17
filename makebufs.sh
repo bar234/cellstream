9a743e2e9141f3284d38af517bd8be41e2ffd766
Ben Rose <bar234@vt.edu>
2009-03-17 16:40:34 -0400

    Updated buffer generation script so it creates the buffers in /tmp, where the program looks by default

--- makebufs.sh
+++ makebufs.sh
@@ -2,7 +2,7 @@
 
 # Example file to generate the buffers
 
-dd if=/dev/zero of=64M_file bs=1048576 count=64
-dd if=/dev/zero of=192M_file bs=1048576 count=192
-dd if=/dev/zero of=384M_file bs=1048576 count=384
+dd if=/dev/zero of=/tmp/64M_file bs=1048576 count=64
+dd if=/dev/zero of=/tmp/192M_file bs=1048576 count=192
+dd if=/dev/zero of=/tmp/384M_file bs=1048576 count=384
 touch outputfile
